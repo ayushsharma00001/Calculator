024e8ba32607310703451019e62faf4da238cc17
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2023-09-10 21:32:13 +0530

    updated calculator with better version

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
 let str = "";
 let input = document.querySelector(".input1");
 let buttons = document.querySelectorAll(".btn");
-input.addEventListener("keypress",(event)=>{
-    console.log(event);
-    
-    // console.log(str);
-    if(event.key == "Enter"){
-        str = eval(str);
-        input.value = str;
-    }
-    else if(event.key == "C" || event.key == "c"){
-        str = "";
-        input.value = str;
-    }
-    else{
-        str = str+event.key;
 
-    }
-    
-});
 buttons.forEach((btn)=>{
     btn.addEventListener("click" , (event)=>{
         if(event.target.innerHTML == "="){
             str = eval(str);
-            input.value = str;
+            input.innerText = str;
         }
         else if(event.target.innerHTML == "C" || event.target.innerHTML == "AC"){
             str = "";
-            input.value = str;
+            input.innerText = 0;
         }
         else{
             str = str + event.target.innerHTML;
-            input.value = str;
+            input.innerText = str;
         }
     });
 });
